5c39e91481a8ecd2e45377645f0aa1bdf173be59
zayweed <91290511+zayweed@users.noreply.github.com>
2022-01-11 13:46:51 -0800

    Update item.h

--- Zuul/item.h
+++ Zuul/item.h
@@ -1,3 +1,4 @@
+//declarations for 'item.cpp'
 #ifndef ITEM_H
 #define ITEM_H
 
@@ -8,10 +9,10 @@
 
 using namespace std;
 
-class item {
+class item { //local variables
     char description[100];
 
-public:
+public: //constructer and functions
     item(char* newDescription);
     char* getDescription();
 };
